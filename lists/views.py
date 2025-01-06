4aa8c883c67a8d0675f39f1409c34ad6f4651c39
Dose <nongdose@gmail.com>
2025-01-06 20:21:59 +0700

    refactor about create objects in views

--- lists/views.py
+++ lists/views.py
@@ -1,7 +1,12 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from lists.models import Item
 
 def home_page(request):
+    if request.method == "POST":
+        Item.objects.create(text=request.POST["item_text"])
+        return redirect("/")
+
     return render(
         request,
         "home.html",
